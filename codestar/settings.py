14d7291fdeb44826bf2c84d2054a3c3b535afad8
EKennard <lizz.kennard@hotmail.com>
2025-07-11 09:51:12 +0100

    settings, gitignore and procfile updated

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2qw4ue3w%0e15tu_9-hm2uw127^daf5uldkgma6_(+=x%dh=!#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [,'.herokuapp.com']
 
 
 # Application definition
