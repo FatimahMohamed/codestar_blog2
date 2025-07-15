426ec8484894d5fcaff7b3ee03d8265923eb2008
EKennard <lizz.kennard@hotmail.com>
2025-07-15 13:11:12 +0100

    code updated to match source code after vscode .venv debug

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User # Import models to connect
-
-STATUS = ((0, "Draft"), (1, "Published"))
+from django.contrib.auth.models import User
 
 # Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -18,23 +19,22 @@ class Post(models.Model):
 
     class Meta:
         ordering = ["-created_on"]
-    
+
     def __str__(self):
         return f"{self.title} | written by {self.author}"
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="commenter")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
     body = models.TextField()
-    approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
-    challenge = models.FloatField(default = 3.0)
+    approved = models.BooleanField(default=False)
 
     class Meta:
         ordering = ["created_on"]
 
     def __str__(self):
-        return f"Comment {self.body} by {self.author}"
+        return f"Comment {self.body} by {self.name}"
