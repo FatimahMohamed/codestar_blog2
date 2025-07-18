dba592f07091f942141dc029b97ff8cf126100af
EKennard <lizz.kennard@hotmail.com>
2025-07-18 15:48:18 +0100

    added authentication

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,6 @@ from django.views import generic
 from .models import Post
 
 # Create your views here.
-
-
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
@@ -31,6 +29,5 @@ def post_detail(request, slug):
     return render(
         request,
         "blog/post_detail.html",
-        {"post": post,
-        "coder": "liz"},
+        {"post": post},
     )
